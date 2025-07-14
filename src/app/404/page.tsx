4824615171840039e95a136a84b2b67fd1ed5577
Forge-Jedaii <forgejedaii@gmail.com>
2025-07-14 21:32:35 +0200

    404

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,10 +1,19 @@
 "use client";
 import Card from "../../components/ui/404ui";
+import Link from "next/link";
 
 export default function Custom404() {
   return (
-    <div className="flex items-center justify-center pt-40">
+    <div className="flex flex-col items-center justify-center ">
       <Card/>
+      <div>
+      <Link 
+        href="/"
+        className="mt-8 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors duration-200 font-medium shadow-md"
+      >
+        Retourner à l&apos;accueil
+      </Link>
+      </div>
     </div>
   );
 }
